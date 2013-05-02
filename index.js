73d1c87e24aa9531fe396d679b1e2c6a2a9b9871
Marshall Rose <mrose17@gmail.com>
2013-05-01 23:00:49 -0700

    prepare for logging configuration

--- index.js
+++ index.js
@@ -613,6 +613,11 @@
         'getStructureId':getFirstStructureId,
         'getStructureIds':getStructureIds,
         'getDeviceIds':getDeviceIds
+        'logger': { error   : function(msg, props) { console.log(msg); console.trace(props.exception); }
+                  , warning : function(msg, props) { console.log(msg); if (props) console.log(props);  }
+                  , notice  : function(msg, props) { console.log(msg); if (props) console.log(props);  }
+                  , info    : function(msg, props) { console.log(msg); if (props) console.log(props);  }
+                  }
     };
 
     nestExports.userAgent = defaultNestUserAgent;
